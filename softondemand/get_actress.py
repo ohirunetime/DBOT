22fc018e1870bc2cb8390f89e79ed8af98530544
ohirunetime <ohirune.timer@gmail.com>
2020-06-10 12:52:24 +0900

    second commit

--- softondemand/get_actress.py
+++ softondemand/get_actress.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+import requests
 
 driver = webdriver.Chrome(executable_path='../chromedriver.exe')
 driver.get('https://www.google.com/')
@@ -8,24 +9,35 @@ driver.get('https://www.google.com/')
 actress = input("女優名を入力 : ")
 page = int(input("ページ数 : "))
 
-time.sleep(10)
-mainBox=driver.find_elements_by_css_selector('div.videis_s_txt h2')
-for i in mainBox :
-    print(i.text)
+time.sleep(3)
 
-
-for i in range(0,page) :
-    url = 'https://ec.sod.co.jp/prime/videos/genre/?search_type=1&sodsearch=' + actress + '&page='+ str(i) + '&sort=2'
+count = 0
+for i in range(0, page):
+    url = 'https://ec.sod.co.jp/prime/videos/genre/?search_type=1&sodsearch=' + \
+        actress + '&page=' + str(i) + '&sort=2'
     driver.get(url)
     time.sleep(10)
-    mainBox=driver.find_elements_by_css_selector('div.videis_s_txt h2')
-    images = driver.find_elements_by_css_selector('div.videis_s_img img')
+    title_Box = driver.find_elements_by_css_selector('div.videis_s_txt h2')
+    image_box = driver.find_elements_by_css_selector('div.videis_s_img img')
+    link_box = driver.find_elements_by_css_selector('div.videis_s_txt a')
 
-    for i in mainBox :
-        print(i.text)
+    for title, image, link in zip(title_Box, image_box, link_box):
+        title = title.text
+        image = image.get_attribute("src")
+        link = link.get_attribute("href")
 
-    for img in images :
-        print(img.text)
+        thumbnail='/static/images'+ actress + str(count)
 
+        print(title, "\n", image, "\n", link)
+
+        file_name = "C:/Users/shinnosuke/Desktop/SODIMAGE/" + actress + str(count)+'.jpg'
+
+        response = requests.get(image,stream=True)
+
+        with open(file_name, 'wb') as f:
+            f.write(response.content)
+
+        count+=1
+        time.sleep(1)
 
 driver.quit()
